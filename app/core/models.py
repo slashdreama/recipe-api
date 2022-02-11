a33f1852367a2daa8c657c9d5061f41f6bc81be6
slashdreama <slashdreama@gmail.com>
2022-02-11 11:51:01 +0000

    Some correction because of Flake8  _2

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+
 # Create your models here.
 class UserManager(BaseUserManager):
 
