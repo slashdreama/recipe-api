ee3ed436212a27ed8afdff0ed0539a1c0be3cb13
slashdreama <slashdreama@gmail.com>
2022-02-11 11:43:25 +0000

    Some correction because of flake8

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
 # Create your models here.
-
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
